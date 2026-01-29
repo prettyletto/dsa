78ecf8a689d00e3dc4a340abc5f8ca2375262144
prettyletto <danielltw2@gmail.com>
2026-01-29 14:53:49 -0300

    sliding window day4

--- arrays/longestsubstring.go
+++ arrays/longestsubstring.go
@@ -1,20 +1,18 @@
-package arrays 
+package arrays
 
+func lengthOfLongestSubstring(s string) int {
+	seen := map[byte]int{}
+	maxLength := 0
 
-func lengthOfLongestSubstring( s string)int {
-	seen := map[byte]bool{}
-	maxLength := 0 
+	l := 0
 
-	leng :=  0
-	for i := range s {
-		if seen[s[i]] {
-			maxLength = max(maxLength, leng)
-			leng = 0
+	for r := range s {
+		if idx, ok := seen[s[r]]; ok && idx >= l {
+			l = idx + 1
 		}
+		seen[s[r]] = r
+		maxLength = max(maxLength, r-l+1)
 	}
-
-
-
 
 	return maxLength
 }
