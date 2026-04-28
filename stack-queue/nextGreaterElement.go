6008cb2625bdd29c3a65876bba95df81c71acf4e
prettyletto <danielltw2@gmail.com>
2026-04-28 10:34:57 -0300

    nextgreaterelement

--- stack-queue/nextGreaterElement.go
+++ stack-queue/nextGreaterElement.go
@@ -2,18 +2,26 @@ package stackqueue
 
 func nextGreaterElement(nums1 []int, nums2 []int) []int {
 	results := make([]int, len(nums1))
-	numToIndex := map[int]int{}
+	stack := []int{}
+	nextGreater := make(map[int]int)
 
-	for _, v := range nums1 {
-		numToIndex[v] = -1
+	for _, num := range nums2 {
+		for len(stack) > 0 && num > stack[len(stack)-1] {
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			nextGreater[top] = num
+		}
+		stack = append(stack, num)
 	}
 
-	for i, v := range nums2 {
-		_, ok := numToIndex[v]
-		if ok {
-			numToIndex[v] = i
-		}
+	for len(stack) > 0 {
+		top := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		nextGreater[top] = -1
+	}
 
+	for i, num := range nums1 {
+		results[i] = nextGreater[num]
 	}
 
 	return results
